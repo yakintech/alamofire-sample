943b0a6e7b3ee04399b0a9eb06172b4a0a406e1a
yakintech <cagatayyildiz87@gmail.com>
2023-11-11 10:47:36 +0300

    Initial Commit

---
+++ AlamofireSample/AlamofireSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireSampleApp.swift
+//  AlamofireSample
+//
+//  Created by Çağatay Yıldız on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
